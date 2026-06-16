2cc35d78ed5f5e2a711fb5500be66e98cdd04803
Diana <diana.menk@mail.ru>
2026-06-16 14:17:58 +0700

    first commit keysolo

--- event-object/keysolo/task.js
+++ event-object/keysolo/task.js
@@ -25,6 +25,22 @@ class Game {
       При неправильном вводе символа - this.fail();
       DOM-элемент текущего символа находится в свойстве this.currentSymbol.
      */
+
+    let inputedSymbol;
+		let current = this;
+
+		function setEnteredSymbol() {
+			let symbol = current.currentSymbol.textContent.toUpperCase();
+			inputedSymbol = String.fromCharCode(event.keyCode);
+
+			if (symbol == inputedSymbol) {
+				current.success();
+			} else {
+				current.fail();
+			}
+		}
+
+		document.addEventListener('keydown', setEnteredSymbol);
   }
 
   success() {
